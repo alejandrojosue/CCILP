52ae2e9f4d00900f054ee90757b0762dc9ea83d2
alejandro <josued.ad13@gmail.com>
2024-10-06 01:30:11 -0600

    deshabilitada barra de herramientas de astro

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -5,6 +5,9 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  devToolbar: {
+    enabled: false
+  },
   server: {
     port: 3000,
     host: '0.0.0.0'
