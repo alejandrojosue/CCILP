3413c7aca74ff8cc5aef58baca873c6131730069
alejandro <josued.ad13@gmail.com>
2024-10-11 21:31:24 -0600

    Correccion de error al momento de validar fecha de expiracion

--- frontend/src/repositories/SupportRepository.ts
+++ frontend/src/repositories/SupportRepository.ts
@@ -8,13 +8,23 @@ export default class SupportRepository {
       const [dataSupport, {sysDate:now}] = await Promise.all([fetchDataFromAPI({url: '/soporte-tecnico'}), fetchDataFromAPI({url: '/soporte-tecnico/systemDate'})])
 
       const {FechaExpiracion, Estado} = dataSupport.data.attributes
-      
-      const difference = parseInt(FechaExpiracion.split('-')[2]) - parseInt(now.split('/')[0])
-      if(difference === 0) msg = 'EXPIRADO'
-      if(difference <= days){
-       msg = `Quedan ${difference} días para que su servicio expire, favor contáctese con soporte técnico para realizar pago.`
+      console.log({FechaExpiracion, now});
+      const nowYear = now.split('/')[2]
+      const nowMonth = now.split('/')[1]
+      const nowDay = now.split('/')[0]
+      const FechaExpiracionYear = FechaExpiracion.split('-')[0]
+      const FechaExpiracionMonth = FechaExpiracion.split('-')[1]
+      const FechaExpiracionDay = FechaExpiracion.split('-')[2]
+
+      const difference = parseInt(FechaExpiracionDay) - parseInt(nowDay)
+      if (FechaExpiracionMonth === nowMonth && FechaExpiracionYear === nowYear){
+        if(difference <= days) {
+          msg = `Quedan ${difference} días para que su servicio expire, favor contáctese con soporte técnico para realizar pago.`
+        }
+        if(difference <= 0) msg = 'EXPIRADO'
       }
     } catch (error) {
+      console.error((error as Error).message);
     }
     return {msg}
   }
