b5176d5b9f82b3fa6a325b1b10038d864e10ef3c
Alejandro Díaz <67884952+alejandrojosue@users.noreply.github.com>
2025-09-30 10:16:04 -0600

    Create astro.config.js

---
+++ frontend/astro.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from "astro/config";
+
+import react from "@astrojs/react";
+
+import node from "@astrojs/node";
+
+// https://astro.build/config
+export default defineConfig({
+  output: 'server',
+  devToolbar: {
+    enabled: false
+  },
+  server: {
+    port: 3001,
+    host: '0.0.0.0'
+  },
+  integrations: [react()],
+  adapter: node({
+    mode: "standalone"
+  })
